4a8ceb7535fb81fd3f321c1e6296fd0582726a67
Morten Haraldsen <m.haraldsen@kezzler.com>
2017-03-10 08:50:26 +0100

    Add license header

--- src/main/java/com/ethlo/time/W3cDateTimeUtil.java
+++ src/main/java/com/ethlo/time/W3cDateTimeUtil.java
@@ -1,5 +1,25 @@
 package com.ethlo.time;
 
+/*-
+ * #%L
+ * Internet Time Utility
+ * %%
+ * Copyright (C) 2017 Morten Haraldsen (ethlo)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.time.OffsetDateTime;
 import java.time.temporal.Temporal;
 
@@ -59,6 +79,7 @@ public interface W3cDateTimeUtil
      * Parse the format with an expected type, i.e sub-class of {@link Temporal}
      * @param s The date/date-time to parse 
      * @param type The type you expect
+     * @param <T> The expected type
      * @return The parsed date/date-time 
      */
     <T extends Temporal> T parseLenient(String s, Class<T> type);
