3577a4fc2dc5a7a1abcec87b0735e9f21f5af244
mha <m.haraldsen@kezzler.com>
2019-08-04 21:47:13 +0200

    Fix tests

--- src/test/java/com/ethlo/time/FieldTest.java
+++ src/test/java/com/ethlo/time/FieldTest.java
@@ -9,9 +9,9 @@ package com.ethlo.time;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,8 @@ package com.ethlo.time;
  * limitations under the License.
  * #L%
  */
+
+import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -35,10 +37,10 @@ public class FieldTest
     @Test
     public void testGetKnownFields()
     {
-        final Field year = Field.valueOf(Year.class);
-        final Field month = Field.valueOf(YearMonth.class);
-        final Field day = Field.valueOf(LocalDate.class);
-        final Field second = Field.valueOf(OffsetDateTime.class);
+        assertThat(Field.valueOf(Year.class)).isEqualTo(Field.YEAR);
+        assertThat(Field.valueOf(YearMonth.class)).isEqualTo(Field.MONTH);
+        assertThat(Field.valueOf(LocalDate.class)).isEqualTo(Field.DAY);
+        assertThat(Field.valueOf(OffsetDateTime.class)).isEqualTo(Field.SECOND);
     }
 
     @Test(expected = IllegalArgumentException.class)
--- src/test/java/com/ethlo/time/ITUTest.java
+++ src/test/java/com/ethlo/time/ITUTest.java
@@ -9,9 +9,9 @@ package com.ethlo.time;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,8 +24,6 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.time.OffsetDateTime;
 import java.time.YearMonth;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
 import java.util.Date;
 
 import org.junit.Test;
