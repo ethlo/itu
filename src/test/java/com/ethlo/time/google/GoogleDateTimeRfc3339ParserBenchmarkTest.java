416a4e0863a68536dd67596553dd550ea5dc35eb
Morten Haraldsen <m.haraldsen@kezzler.com>
2022-03-08 09:34:33 +0100

    Add back test

--- src/test/java/com/ethlo/time/google/GoogleDateTimeRfc3339ParserBenchmarkTest.java
+++ src/test/java/com/ethlo/time/google/GoogleDateTimeRfc3339ParserBenchmarkTest.java
@@ -9,9 +9,9 @@ package com.ethlo.time.google;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,10 +22,10 @@ package com.ethlo.time.google;
 
 import com.ethlo.time.ParserBenchmarkTest;
 
-public class GoogleDateTimeRfc3339ParserBenchmarkTest// extends ParserBenchmarkTest
+public class GoogleDateTimeRfc3339ParserBenchmarkTest extends ParserBenchmarkTest
 {
     public GoogleDateTimeRfc3339ParserBenchmarkTest()
     {
-        //super(new GoogleDateTimeRfc3339());
+        super(new GoogleDateTimeRfc3339());
     }
 }
